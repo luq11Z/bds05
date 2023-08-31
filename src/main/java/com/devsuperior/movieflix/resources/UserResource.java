2caba6a87aac8d90d89f13a3a301df50e0eb5d77
Lucas Silva <lucas_segundinho@hotmail.com>
2023-08-31 18:16:17 +0100

    Fix authorization on user resource

--- src/main/java/com/devsuperior/movieflix/resources/UserResource.java
+++ src/main/java/com/devsuperior/movieflix/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.devsuperior.movieflix.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
+	@PreAuthorize("hasAnyRole('ROLE_MEMBER', 'ROLE_VISITOR')")
 	@GetMapping(value = "/profile")
 	public ResponseEntity<UserDTO> findUserProfile() {
 		UserDTO dto = service.findCurrenUser();
